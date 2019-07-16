680def3fb0847b8a0e8f1f845f458026a242e466
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2019-07-16 22:28:56 +0800

    add sort_humanly()

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import os
-import glob
 import argparse
+import glob
+import os
+import re
+
 import cv2
+
 
 def argument():
     parser = argparse.ArgumentParser(
@@ -17,10 +20,11 @@ def argument():
 def main(args):
     path_image = os.path.join(args.dir_input, "*")
     list_image = glob.glob(path_image)
-    list_image.sort()
+    # list_image.sort()
+    list_image_soted = sort_humanly(list_image)
     os.makedirs(args.dir_output, exist_ok=True)
 
-    for index in range(0, len(list_image), 1):
+    for index in range(0, len(list_image_soted), 1):
         image_first = list_image[index]
         image_second = list_image[index + args.step]
 
@@ -31,6 +35,18 @@ def main(args):
         
         if (index + args.step) > len(list_image):
             break
+
+def tryint(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+def str2int(v_str):
+    return [tryint(sub_str) for sub_str in re.split('([0-9]+)', v_str)]
+
+def sort_humanly(v_list):
+    return sorted(v_list, key=str2int)
 
 if __name__ == "__main__":
     args = argument()
